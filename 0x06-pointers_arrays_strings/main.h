8048558e1553c63dcf75c7beeb565fe11838fd38
Frank <frankmulah250@gmail.com>
2026-07-28 02:23:48 +1400

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,5 +10,7 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
+int _putchar(char c);
+char *_strncpy(char *dest, char *src, int n);
 
 #endif /* MAIN_H */
