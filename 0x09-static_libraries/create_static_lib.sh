821d7e3bee7de5c47f73cf3e39c2013c0efb279e
Frank <frankmulah250@gmail.com>
2026-08-13 05:10:35 +1400

    Add main.h and create_static_lib.sh for static libraries task

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
-gcc -Wall -Werror -Wextra -pedantic -std=gnu89 -c *.c
+# create_static_lib.sh - compile all .c files and pack them into liball.a
+set -euo pipefail
+
+# Remove old library if any
+rm -f liball.a
+
+# Compile each .c to .o with the required flags
+for src in *.c; do
+  [ -e "${src}" ] || continue
+  gcc -c -Wall -Wextra -Werror -pedantic -std=gnu89 "${src}"
+done
+
+# Create static library from all object files
 ar rcs liball.a *.o
+
+# Clean up object files
+rm -f *.o
--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -2,14 +2,15 @@
 #define MAIN_H
 
 int _putchar(char c);
-void _puts(char *str);
 int _islower(int c);
 int _isalpha(int c);
 int _abs(int n);
 int _isupper(int c);
 int _isdigit(int c);
 int _strlen(char *s);
+void _puts(char *s);
 char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -20,6 +21,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-int _atoi(char *s);
 
-#endif
+#endif /* MAIN_H */
