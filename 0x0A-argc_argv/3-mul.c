08a9ba1376bc4a1200daa5a2894c7ce23cde3941
Frank <frankmulah250@gmail.com>
2026-08-17 22:19:54 +1400

    Refactor multiplication logic and improve variable names

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Multiplies two integers passed as arguments and prints the result.
- * If the number of arguments is not 2, prints "Error" and returns 1.
+/**
+ * main - multiplies two numbers passed as arguments
+ * @argc: argument count
+ * @argv: array of pointers to arguments
+ *
+ * Return: 0 on success, 1 on error
  */
 int main(int argc, char *argv[])
 {
-	int a, b, res;
+	int num1, num2, result;
 
 	if (argc != 3)
 	{
 		printf("Error\n");
-		return 1;
+		return (1);
 	}
 
-	a = atoi(argv[1]);
-	b = atoi(argv[2]);
-	res = a * b;
-	printf("%d\n", res);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = num1 * num2;
 
-	return 0;
+	printf("%d\n", result);
+	return (0);
 }
