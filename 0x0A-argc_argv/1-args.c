0885a3adf07d78632badf95b40ddd646ffc783ff
Frank <frankmulah250@gmail.com>
2026-08-17 22:16:48 +1400

    Add function documentation and format return statement

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
-/* Prints the number of arguments passed to the program (not counting program name). */
+/**
+ * main - prints the number of arguments passed into the program
+ * @argc: argument count
+ * @argv: array of pointers to arguments
+ *
+ * Return: Always 0 (Success)
+ */
 int main(int argc, char *argv[])
 {
 	(void)argv;
 	printf("%d\n", argc - 1);
-	return 0;
+	return (0);
 }
