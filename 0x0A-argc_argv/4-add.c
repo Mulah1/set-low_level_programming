21870b765bd2bc3d1cdef4b31fe9ac2d4eecd40d
Frank <frankmulah250@gmail.com>
2026-08-17 22:21:22 +1400

    Refactor main function for adding positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,42 +2,30 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-/* Adds positive numbers provided as arguments.
- * If no numbers are provided, prints 0.
- * If any argument contains non-digit characters, prints "Error" and returns 1.
+/**
+ * main - adds positive numbers passed as arguments
+ * @argc: argument count
+ * @argv: array of pointers to arguments
+ *
+ * Return: 0 on success, 1 on error
  */
 int main(int argc, char *argv[])
 {
-	int i, j;
-	int sum = 0;
-
-	if (argc == 1)
-	{
-		printf("0\n");
-		return 0;
-	}
+	int i, j, sum = 0;
 
 	for (i = 1; i < argc; i++)
 	{
-		/* empty string is invalid */
-		if (argv[i][0] == '\0')
-		{
-			printf("Error\n");
-			return 1;
-		}
-
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (!isdigit((unsigned char)argv[i][j]))
+			if (!isdigit(argv[i][j]))
 			{
 				printf("Error\n");
-				return 1;
+				return (1);
 			}
 		}
-
 		sum += atoi(argv[i]);
 	}
 
 	printf("%d\n", sum);
-	return 0;
+	return (0);
 }
