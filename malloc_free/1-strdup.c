7ccb8ab4a52f24bc746078024ac3c49a6cfed7e6
Frank <frankmulah250@gmail.com>
2026-09-09 01:34:01 +0200

    Refactor _strdup function and update comments
    Updated the _strdup function documentation and improved code readability.

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -3,21 +3,24 @@
 
 /**
  * _strdup - Returns a pointer to a newly allocated space in memory,
- *           containing a copy of the string given as a parameter.
+ *           which contains a copy of the string given as a parameter.
  * @str: The string to duplicate.
  *
- * Return: Pointer to the duplicated string, or NULL if str = NULL or memory fails.
+ * Return: Pointer to the duplicated string, or NULL if str = NULL
+ *         or memory allocation fails.
  */
 char *_strdup(char *str)
 {
 	char *dup;
-	unsigned int len, i;
+	unsigned int len;
+	unsigned int i;
 
 	if (str == NULL)
 		return (NULL);
 
-	for (len = 0; str[len] != '\0'; len++)
-		;
+	len = 0;
+	while (str[len] != '\0')
+		len++;
 
 	dup = malloc(sizeof(char) * (len + 1));
 	if (dup == NULL)
