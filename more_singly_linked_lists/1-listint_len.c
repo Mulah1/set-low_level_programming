5c3e8e665ab075eebcc92dea4bc62880797ee906
Frank <frankmulah250@gmail.com>
2026-09-12 11:57:32 +0200

    Update 1-listint_len.c

--- more_singly_linked_lists/1-listint_len.c
+++ more_singly_linked_lists/1-listint_len.c
@@ -1,16 +1,16 @@
 #include "lists.h"
 
 /**
- * listint_len - Returns the number of elements in a listint_t list.
- * @h: A pointer to the head of the listint_t list.
+ * listint_len - Returns the number of elements in a listint_t list
+ * @h: Pointer to the head of the list
  *
- * Return: The number of elements in the list.
+ * Return: Number of elements in the list, or 0 if NULL
  */
 size_t listint_len(const listint_t *h)
 {
 	size_t count = 0;
 
-	while (h != NULL)
+	while (h)
 	{
 		count++;
 		h = h->next;
