7ae501f443ff79f10982a4f4e9d40863c74011b3
Frank <frankmulah250@gmail.com>
2026-09-21 14:44:43 +0200

    Update 8-sum_listint.c

--- more_singly_linked_lists/8-sum_listint.c
+++ more_singly_linked_lists/8-sum_listint.c
@@ -1,20 +1,28 @@
-#include "lists.h"
+#ifndef LISTS_H
+#define LISTS_H
 
-/**
- * sum_listint - Returns the sum of all the data (n) of a listint_t list.
- * @head: Pointer to the first node of the list.
- *
- * Return: Sum of all integer data, or 0 if the list is empty.
- */
-int sum_listint(listint_t *head)
+#include <stddef.h>
+typedef struct listint_s
 {
-	int sum = 0;
+	int n;
+	struct listint_s *next;
+} listint_t;
 
-	while (head != NULL)
-	{
-		sum += head->n;
-		head = head->next;
-	}
+int _putchar(char c);
+size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
+void free_listint2(listint_t **head);
+int pop_listint(listint_t **head);
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+int sum_listint(listint_t *head);
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
+listint_t *reverse_listint(listint_t **head);
+size_t print_listint_safe(const listint_t *head);
+size_t free_listint_safe(listint_t **h);
+listint_t *find_listint_loop(listint_t *head);
 
-	return (sum);
-}
+#endif /* LISTS_H */
